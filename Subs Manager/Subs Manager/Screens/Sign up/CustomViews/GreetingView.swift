3901a145c99d0fcfbd2f62c70ac43bba898225ad
Kristsina Suvorava <krisvit1999@gmail.com>
2022-11-03 15:58:48 +0200

    SignUpScreen created. XIB views added.

---
+++ Subs Manager/Subs Manager/Screens/Sign up/CustomViews/GreetingView.swift
@@ -0,0 +1,40 @@
+//
+//  GreetingView.swift
+//  Subs Manager
+//
+//  Created by Vlad Birukov on 03.11.2022.
+//
+
+import UIKit
+
+class GreetingView: UIView {
+    let kCONTENT_XIB_NAME = "PersonView"
+    @IBOutlet weak var greetingView: UIView!
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        commonInit()
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        commonInit()
+    }
+
+    func commonInit() {
+        Bundle.main.loadNibNamed(kCONTENT_XIB_NAME, owner: self, options: nil)
+        contentView.setUpGreetingView(self)
+    }
+}
+
+extension UIView {
+    func setUpGreetingView(_ container: UIView) -> Void {
+        self.translatesAutoresizingMaskIntoConstraints = false;
+        self.frame = container.frame;
+        container.addSubview(self);
+        NSLayoutConstraint(item: self, attribute: .leading, relatedBy: .equal, toItem: container, attribute: .leading, multiplier: 1.0, constant: 0).isActive = true
+        NSLayoutConstraint(item: self, attribute: .trailing, relatedBy: .equal, toItem: container, attribute: .trailing, multiplier: 1.0, constant: 0).isActive = true
+        NSLayoutConstraint(item: self, attribute: .top, relatedBy: .equal, toItem: container, attribute: .top, multiplier: 1.0, constant: 0).isActive = true
+        NSLayoutConstraint(item: self, attribute: .bottom, relatedBy: .equal, toItem: container, attribute: .bottom, multiplier: 1.0, constant: 0).isActive = true
+    }
+}
---
+++ Subs Manager/Subs Manager/Screens/Sign up/CustomViews/SignUpView.swift
@@ -0,0 +1,41 @@
+//
+//  SignUpView.swift
+//  Subs Manager
+//
+//  Created by Vlad Birukov on 03.11.2022.
+//
+
+import UIKit
+
+class SignUpView: UIView {
+
+    let kCONTENT_XIB_NAME = "PersonView"
+    @IBOutlet weak var signUpView: UIView!
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        commonInit()
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        commonInit()
+    }
+
+    func commonInit() {
+        Bundle.main.loadNibNamed(kCONTENT_XIB_NAME, owner: self, options: nil)
+        contentView.setUpSignUpView(self)
+    }
+}
+
+extension UIView {
+    func setUpSignUpView(_ container: UIView) -> Void {
+        self.translatesAutoresizingMaskIntoConstraints = false;
+        self.frame = container.frame;
+        container.addSubview(self);
+        NSLayoutConstraint(item: self, attribute: .leading, relatedBy: .equal, toItem: container, attribute: .leading, multiplier: 1.0, constant: 0).isActive = true
+        NSLayoutConstraint(item: self, attribute: .trailing, relatedBy: .equal, toItem: container, attribute: .trailing, multiplier: 1.0, constant: 0).isActive = true
+        NSLayoutConstraint(item: self, attribute: .top, relatedBy: .equal, toItem: container, attribute: .top, multiplier: 1.0, constant: 0).isActive = true
+        NSLayoutConstraint(item: self, attribute: .bottom, relatedBy: .equal, toItem: container, attribute: .bottom, multiplier: 1.0, constant: 0).isActive = true
+    }
+}
