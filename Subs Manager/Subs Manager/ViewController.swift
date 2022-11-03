5badb839803c9ebf30f8a2ce21e70575cc7c399c
Kristsina Suvorava <krisvit1999@gmail.com>
2022-11-03 12:28:08 +0200

    Project created

---
+++ Subs Manager/Subs Manager/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Subs Manager
+//
+//  Created by Vlad Birukov on 03.11.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
