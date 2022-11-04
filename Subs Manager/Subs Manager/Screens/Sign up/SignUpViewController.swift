5b0d7628bc0c9362642b558e813e88ad75bff96f
Kristsina Suvorava <krisvit1999@gmail.com>
2022-11-04 17:09:34 +0200

    .

--- Subs Manager/Subs Manager/Screens/Sign up/SignUpViewController.swift
+++ Subs Manager/Subs Manager/Screens/Sign up/SignUpViewController.swift
@@ -1,44 +1,16 @@
 //
-//  ViewController.swift
+//  RegistrationViewController.swift
 //  Subs Manager
 //
-//  Created by Vlad Birukov on 03.11.2022.
+//  Created by Vlad Birukov on 04.11.2022.
 //
 
 import UIKit
 
-class SignUpViewController: UIViewController, UIScrollViewDelegate {
-
-    @IBOutlet weak var scrollView: UIScrollView!
-    @IBOutlet weak var pageControl: UIPageControl!
-
-    var contentWidth: CGFloat = 0.0
-    let greetingView = GreetingView()
-    let signUpView = SignUpView()
+class SignUpViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        setUpScrollView()
     }
 
-    func scrollViewDidScroll(_ scrollView: UIScrollView) {
-        pageControl.currentPage = Int(scrollView.contentOffset.x / CGFloat(414))
-    }
-
-    private func setUpScrollView() {
-        scrollView.delegate = self
-        scrollView.isPagingEnabled = true
-        scrollView.addSubview(greetingView)
-        scrollView.addSubview(signUpView)
-
-        for i in 0...1 {
-            let xCoordinate = view.frame.width * CGFloat(i)
-            contentWidth += view.frame.width
-            greetingView.frame = CGRect(x: 0, y: 0, width: view.frame.size.width, height: view.frame.size.height)
-            signUpView.frame = CGRect(x: xCoordinate, y: 0, width: view.frame.size.width, height: view.frame.size.height)
-        }
-        
-        scrollView.contentSize = CGSize(width: contentWidth, height: view.frame.height)
-    }
 }
-
