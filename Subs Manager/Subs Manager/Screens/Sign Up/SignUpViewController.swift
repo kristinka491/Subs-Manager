6636bb6286054cc43fea565e9be9c8deb67a9fa1
Kristsina Suvorava <krisvit1999@gmail.com>
2022-11-05 13:16:58 +0200

    Added check whether the textFields in SignUp screen are not empty

--- Subs Manager/Subs Manager/Screens/Sign Up/SignUpViewController.swift
+++ Subs Manager/Subs Manager/Screens/Sign Up/SignUpViewController.swift
@@ -9,8 +9,27 @@ import UIKit
 
 class SignUpViewController: SetUpKeyboardViewController {
 
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var loginTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+
     override func viewDidLoad() {
         super.viewDidLoad()
     }
-    
+
+    private var isNotEmpty: Bool {
+        let nameValue = nameTextField.text
+        let loginValue = loginTextField.text
+        let passwordValue = passwordTextField.text
+        if let nameValue = nameValue,
+            let loginValue = loginValue,
+            let passwordValue = passwordValue  {
+            if !nameValue.isEmpty && !loginValue.isEmpty && !passwordValue.isEmpty {
+                return true
+            } else {
+                return false
+            }
+        }
+        return false
+    }
 }
